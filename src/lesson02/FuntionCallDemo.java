da07b07884ef2b3c5b4ba401b53fe9d88ded9f35
Jun <wangzhen_tju@126.com>
2019-09-28 15:59:21 +0800

    FuntionCallDemo课上练习代码

--- src/lesson02/FuntionCallDemo.java
+++ src/lesson02/FuntionCallDemo.java
@@ -1,10 +1,48 @@
 package lesson02;
 
+import java.util.Scanner;
+
 public class FuntionCallDemo {
+	
+	/**
+	 * 
+	 *  a + b * c
+	 *  
+	 *  第一步: 主函数向calculate 传值a,b,c
+	 *  第二部：calculate调用函数multiple，并得到b*c的值
+	 *  第三部：calculate调用函数add负责a+b*c
+	 *  第四部：calculate将结果返回给main函数
+	 *  第五步: main函数将结果打印
+	 * 
+	 *  备注运算符:  +	-	*	/ %
+	 * 
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		int answer = calculate(a,b,c);
+		System.out.println(answer);
+		
+		
+	}
+	
+	public static int calculate(int a, int b, int c) {
+		int m = multiple(b,c);
+		int result = add(a,m);
+		return result;
+	}
+	
+	public static int multiple(int a, int b) {
+		return a*b;
+	}
+	
+	public static int add(int a, int b) {
+		return a+b;
 	}
 
 }
