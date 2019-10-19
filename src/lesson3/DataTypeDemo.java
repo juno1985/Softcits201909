2dabba3ee07322e5654873efa2d844b4746bb8be
Jun <wangzhen_tju@126.com>
2019-10-19 09:57:25 +0800

    整形的类型一共四种，注意每种取值范围

---
+++ src/lesson3/DataTypeDemo.java
@@ -0,0 +1,26 @@
+package lesson3;
+
+public class DataTypeDemo {
+
+	/**
+	 * 
+	 * byte，占用1个字节(8 bits)（-2的7次方到2的7次方-1） 
+
+		short，占用2个字节(16 bits)（-2的15次方到2的15次方-1）
+
+		int，占用4个字节(32 bits)（-2的31次方到2的31次方-1）
+
+		long，占用8个字节(64 bits)（-2的63次方到2的63次方-1）
+	 */
+	
+	public static void main(String[] args) {
+
+		byte a = -1;
+		byte b = 2;
+		
+		System.out.println((byte)(a+b));
+	
+	}
+	
+
+}
