d776eee85e541c186df631bd848f775e961d45ce
Jun <wangzhen_tju@126.com>
2019-10-19 10:37:34 +0800

    String类型演示

--- src/lesson3/DataTypeDemo.java
+++ src/lesson3/DataTypeDemo.java
@@ -21,12 +21,38 @@ public class DataTypeDemo {
 		a = 1;
 		a = 3;
 		//常量
-		System.out.println(5);
+//		System.out.println(5);
 	
 		short c = 5;
 		//long类型的使用
 		long d = 6666666666666L;
 		
+		
+//		java.lang.Byte.MIN_VALUE;
+		
+		
+		//final 修饰的变量在变量定义后就不能再赋值了
+	//	java.lang.Byte.MAX_VALUE=100;
+		
+		System.out.println(java.lang.Byte.MAX_VALUE);
+		
+		
+		/*
+		 * java.lang.Short.MAX_VALUE; java.lang.Short.MIN_VALUE;
+		 * 
+		 * java.lang.Integer.MAX_VALUE; java.lang.Integer.MIN_VALUE;
+		 * 
+		 * java.lang.Long.MAX_VALUE; java.lang.Long.MIN_VALUE;
+		 */
+		
+		String str1 = "Hello World";
+		
+		String str2 = " konitiwa";
+		
+		String str3 = "abc127ABC*^%%$O))JNB%こにちは你好啊";
+		
+		
+		System.out.println(str1+str2+"               "+str3);
 		
 	}
 	
