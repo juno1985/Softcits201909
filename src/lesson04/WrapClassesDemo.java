727b5c20880ab799fdaf1d67a63fa6029dc32623
Jun <wangzhen_tju@126.com>
2019-11-02 09:58:09 +0800

    8大基本类型对应的封装类型

---
+++ src/lesson04/WrapClassesDemo.java
@@ -0,0 +1,47 @@
+package lesson04;
+
+public class WrapClassesDemo {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * int balance = 0;
+		 * 
+		 * System.out.println(balance);
+		 */
+		/**
+		 * 基本类型的封装类型
+		 * byte - java.lang.Byte
+		 * short - java.lang.Short
+		 * int - java.lang.Integer
+		 * long - java.lang.Long
+		 * boolean - java.lang.Boolean
+		 * char - java.lang.Character
+		 * float - java.lang.Float
+		 * double - java.lang.Double
+		 */
+		
+		Byte b = 11;
+		Short s = 12;
+		Integer i = 13;
+		
+		Long l = null;
+		Long ll = 0L;
+		
+		Boolean bb = true;
+		Character c = 'a';
+		Float f = null;
+		Double d = null;
+		
+		Integer balance = null;
+		
+		if(balance == null) {
+			System.out.println("账户未绑定银行卡");
+		}
+		else {
+			System.out.println("您的账户余额为: ");
+		}
+		
+	}
+
+}
