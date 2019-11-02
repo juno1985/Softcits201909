f5e65af9da6e40feaacc64e2dca7e7d5e40283b4
Jun <wangzhen_tju@126.com>
2019-11-02 09:39:48 +0800

    double不能精确表示小数问题-底层2进制不能精确表示小数，如 1.1

---
+++ src/lesson04/Demo.java
@@ -0,0 +1,15 @@
+package lesson04;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		double a = 2.0;
+		double b = 1.1;
+		
+		double c = a-b;
+		
+		System.out.println(c);
+	}
+
+}
