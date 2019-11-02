881c49408e7a72226204226fe20e0b58962c7a25
Jun <wangzhen_tju@126.com>
2019-11-02 10:58:48 +0800

    数组介绍

---
+++ src/lesson04/ArrayDemo.java
@@ -0,0 +1,19 @@
+package lesson04;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		
+		int[] a_array = new int[10];
+		//数组中的第一个元素赋值
+		a_array[0] = 1111;
+		//数组中的第二个元素赋值
+		a_array[1] = 2222;
+		//数组中的第十个元素赋值
+		a_array[9] = 9999;
+		
+		a_array[10] = 1111;
+
+	}
+
+}
