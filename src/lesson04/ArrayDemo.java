3141eb139a2d480242183a74508a6ae91d5e54ad
Jun <wangzhen_tju@126.com>
2019-11-02 11:02:42 +0800

    数组声明定义同时完成

--- src/lesson04/ArrayDemo.java
+++ src/lesson04/ArrayDemo.java
@@ -4,16 +4,18 @@ public class ArrayDemo {
 
 	public static void main(String[] args) {
 		
-		int[] a_array = new int[10];
-		//数组中的第一个元素赋值
-		a_array[0] = 1111;
-		//数组中的第二个元素赋值
-		a_array[1] = 2222;
-		//数组中的第十个元素赋值
-		a_array[9] = 9999;
-		
-		a_array[10] = 1111;
+		/*
+		 * String[] a_array = new String[10]; //数组中的第一个元素赋值 a_array[0] = "1111";
+		 * //数组中的第二个元素赋值 a_array[1] = "2222"; //数组中的第十个元素赋值 a_array[9] = "9999";
+		 * 
+		 * a_array[10] = "1111aaafjad99nr98ea7f9$%%^&()_$%#";
+		 */
 
+		
+		Integer[] a_array = {1,3,5,7,9,2,4,6,8,10};	
+		
+		
+		
 	}
 
 }
