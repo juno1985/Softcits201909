629e0b6a813c9f947b627b6a32a0a593e5d20ff5
Jun <wangzhen_tju@126.com>
2019-11-09 14:55:00 +0800

    TimeInWords 提示

---
+++ src/lesson5/TimeInWords.java
@@ -0,0 +1,73 @@
+package lesson5;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ *  String[] numbers = {
+        "zero", 
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty",
+        "twenty one",
+        "twenty two",
+        "twenty three",
+        "twenty four",
+        "twenty five",
+        "twenty six",
+        "twenty seven",
+        "twenty eight",
+        "twenty nine"
+    };
+ *
+ */
+public class TimeInWords {
+
+    // Complete the timeInWords function below.
+    static String timeInWords(int h, int m) {
+
+    	7 -> seven
+    	
+    	numbers[7];
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int h = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int m = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String result = timeInWords(h, m);
+
+        System.out.println(result);
+        
+        scanner.close();
+    }
+}
