542f73d1a73be337eb8ec9f33c9de3ae7185450c
Jun <wangzhen_tju@126.com>
2019-11-09 09:05:09 +0800

    while 循环演示

---
+++ src/lesson5/WhileDemo.java
@@ -0,0 +1,16 @@
+package lesson5;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		//死循环
+		while(i < 5) {
+			System.out.println("--> " + ++i);
+		
+		}
+
+	}
+
+}
