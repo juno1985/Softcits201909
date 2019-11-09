56b2088c790b9c70e82079672e06a4409f0a017e
Jun <wangzhen_tju@126.com>
2019-11-09 09:08:20 +0800

    DoWhile循环体

---
+++ src/lesson5/DoWhileDemo.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		
+		int i =6;
+		
+		do {
+			System.out.println("--> " + i);
+		}while(i<5);
+
+	}
+
+}
