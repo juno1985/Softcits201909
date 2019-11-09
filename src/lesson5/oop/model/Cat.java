7bf1ccc72b8a4e82dccdf15791f41fbaa94707ca
Jun <wangzhen_tju@126.com>
2019-11-09 11:44:59 +0800

    创建类时直接快捷创建继承和接口

---
+++ src/lesson5/oop/model/Cat.java
@@ -0,0 +1,12 @@
+package lesson5.oop.model;
+
+import lesson5.oop.action.AbstractAction;
+
+public class Cat extends Animal implements AbstractAction {
+
+	
+	public void catchMouse() {
+		
+	}
+	
+}
