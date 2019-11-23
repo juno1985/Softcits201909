0685e33f00516f16dfdba611a95a56cb2418afd9
Jun <wangzhen_tju@126.com>
2019-11-23 16:16:52 +0800

    for循环遍历

---
+++ src/lesson6/Solution.java
@@ -0,0 +1,16 @@
+package lesson6;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int[] arr = { 3, 6, 9 };
+
+		for (int i : arr)
+			System.out.println(i);
+
+		String[] str = { "abc", "def", "hig" };
+		for (String s : str)
+			System.out.println(s);
+	}
+
+}
