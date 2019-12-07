8e13abde0453ad612d2e6307d3642f4fe62d3121
Jun <wangzhen_tju@126.com>
2019-12-07 09:32:01 +0800

    static修饰的变量属于类级别，所以所有的对象都共有该变量

---
+++ src/lesson7/Demo.java
@@ -0,0 +1,16 @@
+package lesson7;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		Person p1 = new Person();
+		Person p2 = new Person();
+		
+		System.out.println(p1.getId());
+		System.out.println(p2.getId());
+
+	}
+
+}
--- src/lesson7/Person.java
+++ src/lesson7/Person.java
@@ -1,11 +1,43 @@
 package lesson7;
 
 public class Person implements Creature {
+	//static修饰的变量属于类级别，所以所有的对象都共有该变量
+	private static Integer num = 0;
+	
+	private Integer id;// 1,2,3....
+	private String name;
+	
+	public Person() {
+		this.setId(++num);
+	}
+
+	public Person(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 	@Override
 	public void walk() {
 		// TODO Auto-generated method stub
 
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
