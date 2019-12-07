1900cfd9bb0812a2809fe5a0e90166043aee8cdb
Jun <wangzhen_tju@126.com>
2019-12-07 09:19:45 +0800

    final修饰类演示

--- src/lesson7/final_demo/FinalDemo.java
+++ src/lesson7/final_demo/FinalDemo.java
@@ -1,11 +1,7 @@
 package lesson7.final_demo;
 
-public class FinalDemo {
+public final class FinalDemo {
 	
-	final String str = "abc";
-	
-	public final void changeStr(String a) {
-		System.out.println(a);
-	}
+
 
 }
--- src/lesson7/final_demo/SubFinalDemo.java
+++ src/lesson7/final_demo/SubFinalDemo.java
@@ -2,7 +2,5 @@ package lesson7.final_demo;
 
 public class SubFinalDemo extends FinalDemo {
 
-	public  void changeStr(String a) {
-		System.out.println("方法重写后: " + a);
-	}
+	
 }
