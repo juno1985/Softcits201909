3500aba57d4ef3fa0f022d21054b531324e37e5b
Jun <wangzhen_tju@126.com>
2019-12-07 12:04:01 +0800

    选择排序 算法复杂度测量

---
+++ src/lesson7/MySortAndFindUtil.java
@@ -0,0 +1,41 @@
+package lesson7;
+
+public final class MySortAndFindUtil {
+
+	/**
+	 * 
+	 * 选择排序， 就是每次选择出一个最大/最小的数
+	 * 一共遍历 N次， N为数组的长度
+	 * 
+	 * 算法复杂度：
+	 * O(N*N)
+	 * 
+	 */
+	public static Integer[] mySort(Integer[] arr) {
+		
+		Integer[] result = new Integer[arr.length];
+		Integer result_index = 0;
+		Integer min = Integer.MAX_VALUE;
+		
+		Integer index = 0;
+		//一共从头走到尾的次数
+		for(int j = 0; j < arr.length; j++) {
+			//从头走到尾
+			for(int i = 0; i < arr.length; i++) {
+				if(arr[i] != null && arr[i] < min) {
+					//标记当前下标，扫描一遍后会记null
+					index = i;
+					//更新当前最小值
+					min = arr[i];
+				}
+			}
+			
+			arr[index] = null;
+			result[result_index++] = min;
+			min = Integer.MAX_VALUE;
+		}
+		
+		return result;
+	}
+	
+}
---
+++ src/lesson7/MySortDemo.java
@@ -0,0 +1,36 @@
+package lesson7;
+
+import java.util.Random;
+
+public class MySortDemo {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random ();
+		
+		Integer[] toBeSortArr = new Integer[100000];
+		//初始化一个待排序的数组
+		for(int i = 0; i < toBeSortArr.length; i++)
+			toBeSortArr[i] = random.nextInt(Integer.MAX_VALUE);
+		//排序前是这个样子的
+		for(Integer t : toBeSortArr)
+			System.out.println(t);
+		
+		System.out.println("-----------华丽的分隔符-----------");
+		
+		Long start_time = System.currentTimeMillis();
+		
+		//调用排序方法
+		toBeSortArr = MySortAndFindUtil.mySort(toBeSortArr);
+		
+		Long end_time = System.currentTimeMillis();
+		
+		//遍历排序后的方法并输出
+		for(Integer t : toBeSortArr)
+			System.out.println(t);
+		
+		System.out.println("排序用时: " + (end_time-start_time)/1000 + "秒");
+		
+	}
+
+}
