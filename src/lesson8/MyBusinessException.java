29af02d39ca97025e4e18fcde20a856ba8a8891d
Jun <wangzhen_tju@126.com>
2019-12-14 12:01:23 +0800

    自定义异常

---
+++ src/lesson8/MyBusinessException.java
@@ -0,0 +1,11 @@
+package lesson8;
+
+public class MyBusinessException extends RuntimeException {
+
+	public MyBusinessException(String message) {
+		super(message);
+	}
+
+	
+
+}
--- src/lesson8/MyExceptionDemo.java
+++ src/lesson8/MyExceptionDemo.java
@@ -12,6 +12,9 @@ public class MyExceptionDemo {
 			myexp.a = Integer.parseInt(args[0]);
 			myexp.b = Integer.parseInt(args[1]);
 			myexp.c = myexp.a / myexp.b;
+			
+			if(myexp.c < 0) throw new MyBusinessException("平均营业额不能为负数");
+			
 		} catch (ArithmeticException ae) {
 			// ae.printStackTrace();
 			System.out.println("被除数不能为零");
@@ -19,9 +22,13 @@ public class MyExceptionDemo {
 			System.out.println("输入个数不为2个");
 		} catch (NumberFormatException nfe) {
 			System.out.println("输入不如数字");
-		} catch(Exception ex) {
+		} catch (MyBusinessException mbe) {
+			mbe.printStackTrace();
+		}
+		catch(Exception ex) {
 			System.out.println("未知异常");
-		} finally {
+		} 
+		finally {
 			System.out.println("异常抛出后,finally一定会执行");
 		}
 
