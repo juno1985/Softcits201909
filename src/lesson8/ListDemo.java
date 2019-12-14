4c310b6becda3c8c8576e5c451d62a3e434fea78
Jun <wangzhen_tju@126.com>
2019-12-14 10:48:18 +0800

    ArrayList: 扩容机制！！！！！！

---
+++ src/lesson8/ListDemo.java
@@ -0,0 +1,34 @@
+package lesson8;
+
+import java.util.ArrayList;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> str_arr = new ArrayList<String>();
+
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		str_arr.add("abc");
+		System.out.println("当前长度： " + str_arr.size());
+		
+	}
+
+}
