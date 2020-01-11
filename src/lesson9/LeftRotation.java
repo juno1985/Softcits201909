10968002f7e5d25c2cd6c81678e1f39872e8d4eb
Jun <wangzhen_tju@126.com>
2020-01-11 14:48:52 +0800

    DayoftheProgrammer

--- src/lesson9/LeftRotation.java
+++ src/lesson9/LeftRotation.java
@@ -13,6 +13,8 @@ public class LeftRotation {
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		
+		
 		String[] nd = scanner.nextLine().split(" ");
 
 		int n = Integer.parseInt(nd[0]);
@@ -29,6 +31,12 @@ public class LeftRotation {
 			a[i] = aItem;
 		}
 
+		
+		
+		long start_time = System.currentTimeMillis();
+		
+		d %= n;
+		
 		int temp = 0;
 		for (int i = 0; i < d; i++) {
 			//将第一个元素保存在temp中
@@ -40,10 +48,15 @@ public class LeftRotation {
 			//数组最后一个元素为第一个元素值即temp
 			a[n - 1] = temp;
 		}
+		
+		long end_time = System.currentTimeMillis();
+		
 		for (int i = 0; i < n; i++) {
 
 			System.out.print(a[i] + " ");
 		}
+		
+		System.out.println("共运行:  " + (end_time-start_time)/1000 + " 秒");
 
 		scanner.close();
 	}
