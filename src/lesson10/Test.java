45cd815192b8d35d8d1223dd9ee4159c30ee1670
Jun <wangzhen_tju@126.com>
2020-01-18 09:40:30 +0800

    静态内部类对象可以直接生成，不需要通过外部类对象

--- src/lesson10/Test.java
+++ src/lesson10/Test.java
@@ -9,8 +9,10 @@ public class Test {
 		
 		//内部类对象的生成是通过外部类对象生成的
 		Outer out = new Outer();
+		//静态内部类对象可以直接生成，不需要通过外部类对象
+		Outer.Inner in = new Outer.Inner();
 		
-		Outer.Inner in = out.new Inner();
+	//	Outer.Inner in1 = new Outer().new Inner();
 		
 		System.out.println(out.a);
 		
