6aa687b092294a52742fa833fd998b93bcf02d82
Jun <wangzhen_tju@126.com>
2020-04-18 15:35:46 +0800

    Java Stdin and Stdout I

---
+++ src/catchup_1/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,16 @@
+package catchup_1;
+import java.util.*;
+public class Java_Stdin_and_Stdout_I {
+
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int i = 1;
+	        while(i <=3) {
+	        	int a = scan.nextInt();
+	 	        System.out.println(a);
+	 	        i++;
+	        }
+	       
+	    }
+	
+}
