f040bca73b71311e2e6dff70aa3c6f1fa756c5a1
Jun <wangzhen_tju@126.com>
2020-07-11 13:15:51 +0800

    简单的函数 带返回值return

---
+++ src/catchup_2/MyFunction.java
@@ -0,0 +1,17 @@
+package catchup_2;
+
+public class MyFunction {
+
+	public static void main(String[] args) {
+
+		int result = add(2,5);
+		
+		System.out.println(result);
+	}
+	
+	//a+b
+	static int add(int a, int b) {
+		return a + b;
+	}
+
+}
