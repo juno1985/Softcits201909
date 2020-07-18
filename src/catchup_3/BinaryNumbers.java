b40866b622f34857db160c0196a36e30773d3d80
Jun <wangzhen_tju@126.com>
2020-07-18 15:31:27 +0800

    BinaryNumbers

---
+++ src/catchup_3/BinaryNumbers.java
@@ -0,0 +1,36 @@
+package catchup_3;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class BinaryNumbers {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        
+        
+       String binary = Integer.toBinaryString(n);
+       
+       
+       int t = 0;
+       
+       "101001001101010101010"
+       
+       if(arr[i] == '1') t++;
+       else {
+    	   t=0;
+    	   t > max 
+       }
+    }
+}
