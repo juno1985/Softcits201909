8458ca5a5b45e417eb1cc5251a718a99363da430
Jun <wangzhen_tju@126.com>
2020-08-15 10:22:01 +0800

    带构造器参数的枚举类

---
+++ src/catchup_4/Gender.java
@@ -0,0 +1,18 @@
+package catchup_4;
+
+
+public enum Gender
+{
+	//此处的枚举值必须调用对应构造器来创建
+	MALE("男"),FEMALE("女");
+	private final String name;
+	//枚举类的构造器只能使用private修饰
+	private Gender(String name)
+	{
+		this.name = name;
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+}
---
+++ src/catchup_4/GenderTest.java
@@ -0,0 +1,12 @@
+package catchup_4;
+
+public class GenderTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Gender.MALE.toString());
+		System.out.println(Gender.MALE.getName());
+
+	}
+
+}
