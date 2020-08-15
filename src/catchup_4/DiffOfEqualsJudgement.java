e9c3d8754d2bf5e988e3efb798471867945bafb4
Jun <wangzhen_tju@126.com>
2020-08-15 12:18:53 +0800

    String和StringBuilder的区别

--- src/catchup_4/DiffOfEqualsJudgement.java
+++ src/catchup_4/DiffOfEqualsJudgement.java
@@ -14,6 +14,35 @@ public class DiffOfEqualsJudgement {
 		int a = 1;
 		int b = 1;
 		System.out.println(a==b);
+		
+		String str1 = new String("abc");
+		String str2 = new String("abc");
+		
+		System.out.println(str1==str2); // true	//false
+		System.out.println(str1.equals(str2)); // false	//true
+		
+		System.out.println(str1.hashCode());
+		
+		System.out.println("p1 hash code: " + p1.hashCode());
+		p1 = new Person("Alex");
+		System.out.println("p1 hash code: " + p1.hashCode());
+		
+		
+		
+		System.out.println("str1 hash code: " + str1.hashCode());
+		str1 += "def";
+		System.out.println("str1 hash code: " + str1.hashCode());
+		
+		System.out.println("-------------------------");
+		
+		StringBuilder sb = new StringBuilder();
+		System.out.println(sb.hashCode());
+		sb.append("abc");
+		System.out.println(sb.hashCode());
+		System.out.println(sb.toString());
+		sb.append("def");
+		System.out.println(sb.hashCode());
+		System.out.println(sb.toString());
 
 	}
 
--- src/catchup_4/Person.java
+++ src/catchup_4/Person.java
@@ -10,6 +10,12 @@ public class Person {
 	
 	
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
 	public String getName() {
 		return name;
 	}
