b57a25790ba6f884306c796493318cbb4a1eb150
Jun <wangzhen_tju@126.com>
2020-08-15 13:51:06 +0800

    迭代的方式

--- src/catchup_4/Recursion.java
+++ src/catchup_4/Recursion.java
@@ -1,6 +1,5 @@
 package catchup_4;
 
-
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -11,35 +10,25 @@ import java.util.regex.*;
 
 public class Recursion {
 
-    // Complete the factorial function below.
-    static int factorial(int n) {
-    	int result = 1;
-    	
-        
-    	for(int i = 2; i <=n; i++) {
-    		
-    		result = i * result;
-    	}
-    	
-    	return result;
+	// Complete the factorial function below.
+	static int factorial(int n) {
+		if (n == 1)
+			return 1;
+		else
+			return factorial(n - 1) * n;
+	}
 
-    }
+	private static final Scanner scanner = new Scanner(System.in);
 
-    private static final Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+		int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		int result = factorial(n);
 
-        int result = factorial(n);
+		System.out.println(result);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
-    }
+		scanner.close();
+	}
 }
