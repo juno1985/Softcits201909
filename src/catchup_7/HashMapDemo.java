55c645914657cffd44aabf2c2c12d50ec605b033
Jun <wangzhen_tju@126.com>
2020-09-12 10:12:41 +0800

    HashMap底层讲解

---
+++ src/catchup_7/HashMapDemo.java
@@ -0,0 +1,18 @@
+package catchup_7;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> hashMap = new HashMap<>();
+		
+		hashMap.put(1, "Mark");
+		hashMap.put(1, "Alex");
+		
+		
+	}
+
+}
