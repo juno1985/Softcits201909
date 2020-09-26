c3680cf065d31193f514105cc48a1985c7cdab8f
Jun <wangzhen_tju@126.com>
2020-09-26 11:44:56 +0800

    Dictionaries and Maps

---
+++ src/catchup_9/DictionariesandMaps.java
@@ -0,0 +1,27 @@
+package catchup_9;
+
+
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class DictionariesandMaps{
+  public static void main(String []argh){
+	  
+	  Map<String, Integer> map = new HashMap<>();
+	  
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+      for(int i = 0; i < n; i++){
+          String name = in.next();
+          int phone = in.nextInt();
+          // Write code here
+          map.put(name, phone);
+      }
+      while(in.hasNext()){
+          String s = in.next();
+          // Write code here
+      }
+      in.close();
+  }
+}
