f9b2d508cf74401610b99c4de01a31f4d3e5b00f
Jun <wangzhen_tju@126.com>
2020-09-26 13:42:13 +0800

    Inheritance

---
+++ src/catchup_9/Inheritance.java
@@ -0,0 +1,85 @@
+package catchup_9;
+import java.util.*;
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+
+class Student extends Person{
+	private int[] testScores;
+
+	public Student(String firstName, String lastName, int identification,
+			int[] testScores) {
+		super(firstName, lastName, identification);
+		this.testScores = testScores;
+	}
+
+	public String calculate() {
+		int sum = 0;
+		float average;
+		for(int t : testScores)
+			sum += t;
+		
+		average = sum * 1.0f/ testScores.length;
+		
+		if(average < 40) return "T";
+		else if(average < 55) return "D";
+		else if(average < 70) return "P";
+		else if(average < 80) return "A";
+		else if(average < 90) return "E";
+		else return "O";
+	}
+
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+	
+}
+
+class Inheritance {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate() );
+	}
+}
