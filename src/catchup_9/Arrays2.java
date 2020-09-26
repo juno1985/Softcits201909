863486f61c498f476e3c09c39999bd64573efa20
Jun <wangzhen_tju@126.com>
2020-09-26 11:29:54 +0800

    Arrays - Stack

--- src/catchup_9/Arrays2.java
+++ src/catchup_9/Arrays2.java
@@ -13,29 +13,26 @@ public class Arrays2 {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    	  int n = scanner.nextInt();
+          
+          scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
 
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+          String[] arrItems = scanner.nextLine().split(" ");
+          scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        Stack<Integer> stack = new Stack<>();
-        
-        
-        for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            stack.push(arrItem);
-        }
-        
-        
-        while(!stack.isEmpty())
-        	System.out.println(stack.pop());
-        
-
-        scanner.close();
-      
-    }
+          Stack<Integer> stack = new Stack<>();
+          
+          
+          for (int i = 0; i < n; i++) {
+              int arrItem = Integer.parseInt(arrItems[i]);
+              stack.push(arrItem);
+          }
+          
+          
+          while(!stack.isEmpty())
+              System.out.print(stack.pop()+" ");
+      }
 
   
 }
