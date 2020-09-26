0184547b11ab3411fe47e8f4943cfbd77dd861fa
Jun <wangzhen_tju@126.com>
2020-09-26 15:33:55 +0800

    Exceptions - String to Integer

---
+++ src/catchup_9/ExceptionsStringtoInteger.java
@@ -0,0 +1,21 @@
+package catchup_9;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class ExceptionsStringtoInteger {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        
+        try {
+			System.out.println(Integer.parseInt(S));
+		} catch (Exception e) {
+			System.out.println("Bad String");
+		}
+    }
+}
