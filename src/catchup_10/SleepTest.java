95c2a19a9a5ae02c8b16874aaf7ae81172fa24d9
Jun <wangzhen_tju@126.com>
2020-10-17 15:06:53 +0800

    演示死循环

--- src/catchup_10/SleepTest.java
+++ src/catchup_10/SleepTest.java
@@ -6,10 +6,8 @@ public class SleepTest
 	public static void main(String[] args)
 		throws Exception
 	{
-		for (int i = 0; i < 10 ; i++ )
-		{
-			System.out.println("当前时间: " + new Date());
-			// 调用sleep方法让当前线程暂停1s。
+		while(true) {
+			System.out.println("hello");
 			Thread.sleep(1000);
 		}
 	}
