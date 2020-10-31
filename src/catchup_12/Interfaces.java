a4af19ae91fb902fc0c225547ce136efe3c28bb3
Jun <wangzhen_tju@126.com>
2020-10-31 15:55:29 +0800

    第二种方式

--- src/catchup_12/Interfaces.java
+++ src/catchup_12/Interfaces.java
@@ -15,8 +15,11 @@ class Calculator implements AdvancedArithmetic {
     }
     public List<Integer> getEvenlyDivisors(int n){
     	List<Integer> list = new ArrayList<Integer>();
-    	for(int i = 1; i <= n; i++) {
-    		if(n % i == 0) list.add(i);
+    	for(int i = 1; i <= Math.sqrt(n); i++) {
+    		if(n % i == 0 ) {
+    			list.add(i);
+    			if(i != n/i) list.add(n / i);
+    		}
     	}
     	return list;
     }
