cccda201362904c9787db3ec482c2dad7ce4dc0b
Jun <wangzhen_tju@126.com>
2020-11-07 09:55:19 +0800

    使用线程池提交FutureTask

--- src/catachup_13/MyCallableTest.java
+++ src/catachup_13/MyCallableTest.java
@@ -1,11 +1,15 @@
 package catachup_13;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
 public class MyCallableTest {
 
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
+	public static void main(String[] args)
+			throws InterruptedException, ExecutionException {
 		// 创建callable的实现类对象
 		MyCallable myCallable = new MyCallable();
 		// 用futuretask包裹callable的返回值, futuretask实际上是把callable转化为runnable
@@ -15,9 +19,37 @@ public class MyCallableTest {
 		// 运行线程
 		thread.start();
 		// 获取线程返回的结果, futureTask的get（）方法会阻塞主线程
-		Integer result = (Integer)futureTask.get();
+		Integer result = (Integer) futureTask.get();
+
+		System.out.println("将FutureTask提交到新建线程,返回结果: " + result);
+
+		FutureTask<Integer> futureTask1 = new FutureTask<>(
+				new Callable<Integer>() {
+
+					@Override
+					public Integer call() throws Exception {
+						int sum = 0;
+						for (int i = 1; i <= 10; i++)
+							sum += i;
+						return sum;
+					}
+
+				});
+
+		ExecutorService es = Executors.newCachedThreadPool();
+		es.submit(futureTask1);
+
+		Integer result1 = (Integer) futureTask1.get();
+		System.out.println("将FutureTask提交到线程池,返回结果: " + result1);
+
+		//Lambda表达式
+		FutureTask<Integer> futureTask2 = new FutureTask<>(() -> {
+			int sum = 0;
+			for (int i = 1; i <= 10; i++)
+				sum += i;
+			return sum;
+		});
 		
-		System.out.println(result);
 
 	}
 
