95861d1506fa6bc6628c789cc355159884774e82
Jun <wangzhen_tju@126.com>
2020-11-14 10:53:12 +0800

    FileOutputStream

--- src/catchup_14/ManualInput.java
+++ src/catchup_14/ManualInput.java
@@ -1,5 +1,7 @@
 package catchup_14;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
@@ -15,8 +17,16 @@ public class ManualInput {
 		 */
 		
 		byte[] buff = new byte[1024];
-		
+		FileOutputStream fileOutput = null;
 		try {
+			
+			File file = new File("keyboardinput.txt");
+			
+			if(!file.exists()) file.createNewFile();
+			
+			//得到文件的输出流
+			fileOutput = new FileOutputStream(file);
+			
 			int count;
 			//ctrl+z 会发出eof 即输入关闭信号，此时count = -1
 			while((count = inputStream.read(buff)) != -1) {
@@ -25,10 +35,21 @@ public class ManualInput {
 				String str = new String(buff, 0, count);
 				//屏幕输出键盘输入的内容
 				System.out.println("读到键盘输入: " + str);
+				
+				//写入文件
+				fileOutput.write(buff, 0, count);
 			}
 		} catch (IOException e) {
 			
 			e.printStackTrace();
+		} finally {
+			try {
+				inputStream.close();
+				if(fileOutput != null)
+					fileOutput.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		
