ec4d3e7c9c14dd0515ad8aa61b4a3321fa79e621
Jun <wangzhen_tju@126.com>
2021-03-13 10:58:03 +0800

    PreparedStatement

--- src/catchup_20/SalesAdd.java
+++ src/catchup_20/SalesAdd.java
@@ -1,7 +1,9 @@
 package catchup_20;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,11 +18,23 @@ public class SalesAdd {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://192.168.1.108:3306/sales", "root", "123456");
-			stmt = conn.createStatement();
+			//conn.setAutoCommit(false);
+			//stmt = conn.createStatement();
+			PreparedStatement preparedStat = conn.prepareStatement("insert into product_tbl values(null, ?, ?, ?, ?, ?)");
+
+			preparedStat.setString(1, "Lining");
+			preparedStat.setDouble(2, 89.00);
+			Date date = new Date(120, 2, 15);
+			preparedStat.setDate(3, date);
+			preparedStat.setString(4, "HangZhou");
+			preparedStat.setInt(5, 3);
+			
 			// turn off auto commit
-			stmt.execute("SET autocommit=0;"); 
-			int add_rows = stmt.executeUpdate("insert into product_tbl values(null, '361du', 99.99, '2012-01-02', 'ShenYang', 3);");
-			stmt.execute("commit;");
+			//stmt.execute("SET autocommit=0;"); 
+			//int add_rows = stmt.executeUpdate("insert into product_tbl values(null, '361du', 99.99, '2012-01-02', 'ShenYang', 3);");
+			int add_rows = preparedStat.executeUpdate();
+			//stmt.execute("commit;");
+			//conn.commit();
 			System.out.println("Row Num Added: " + add_rows);
 			
 		} catch (ClassNotFoundException e) {
